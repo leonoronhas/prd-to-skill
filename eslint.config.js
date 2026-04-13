dd1f203b85654339eaf7223598dd0d39d4d56fe2
leonoronhas <leonoronsantos@gmail.com>
2026-04-13 14:15:07 -0600

    chore: Add ESLint configuration with TypeScript and Prettier integration

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import prettier from "eslint-config-prettier";
+
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.strict,
+  prettier,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    },
+  },
+  {
+    ignores: ["dist/", "node_modules/", "coverage/", "*.config.*"],
+  },
+);
